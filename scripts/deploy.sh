bb884b633a2d82514cf65dadfb0162723494fbcd
MajorTwip <majortwip@twipnet.ch>
2019-09-21 21:20:15 +0200

    mend

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build
+docker build -t majortwip/escapeconnect:latest
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push majortwip/escapeconnect
