88f5a1a7b3e16257629b0062dc4713927537f9dc
MajorTwip <majortwip@twipnet.ch>
2019-09-22 11:40:10 +0200

    Now copy war-File to Docker-Image

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-pwd
-ls -la
+cp target/*.war /usr/local/tomcat/webapps/ROOT.war
 docker build -t majortwip/escapeconnect:latest .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push majortwip/escapeconnect
