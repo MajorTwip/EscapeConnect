a0807cbecc14f66128726548c433868c93974b17
MajorTwip <majortwip@twipnet.ch>
2019-09-22 11:52:37 +0200

    Working Hello World

--- src/ch/ffhs/pa5/escapeconnect/server/DashboardAPI.java
+++ src/ch/ffhs/pa5/escapeconnect/server/DashboardAPI.java
@@ -1,8 +1,19 @@
 package ch.ffhs.pa5.escapeconnect.server;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/dashboard")
 public class DashboardAPI {
+	
+	@Path("/get")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getUser() {
+		return Response.status(200).entity("Hello World").build();
+	}
 	
 }
