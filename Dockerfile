67e987e995103fa9199c185e33c8e4f55fa72934
MajorTwip <majortwip@twipnet.ch>
2019-09-30 17:34:12 +0200

    Implement SwaggerUI to be served at /doc

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,8 @@ FROM tomcat:9
 RUN rm -rf /usr/local/tomcat/webapps/*
 COPY ROOT.war /usr/local/tomcat/webapps/
 
+RUN mkdir /usr/local/tomcat/webapps/doc
+COPY swagger-dist/* /usr/local/tomcat/webapps/doc
+
 RUN mkdir /data
 VOLUME /data
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp target/*.war ./ROOT.war
+cp schemas/api.yaml swagger-dist/api.yaml
 docker build -t majortwip/escapeconnect:latest .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push majortwip/escapeconnect
