a8588191954a69a239b8e5031b9ee6acd7c23dde
MajorTwip <majortwip@twipnet.ch>
2019-10-23 22:48:45 +0200

    demo angepasst

--- src/ch/ffhs/pa5/escapeconnect/handlers/PanelAPIimplement.java
+++ src/ch/ffhs/pa5/escapeconnect/handlers/PanelAPIimplement.java
@@ -18,38 +18,14 @@ public class PanelAPIimplement implements PanelApiService {
 		// TODO Auto-generated method stub
 		// Demo
 		
-		List<Panel> panels = new ArrayList<>();
-		Panel panel1 = new Panel();
-		panel1.setId(1);
-		panel1.setStatus(true);
-		panel1.setTitle("Sandrätsel");
-		Action action = new Action();
-		action.setId(1);
-		action.setLabel("Freischalten");
-		panel1.getActions().add(action);
-		Value val = new Value();
-		val.setId(2);
-		val.setLabel("Sandlevel");
-		val.setValue("50%");
-		panel1.getValues().add(val);
-		panels.add(panel1);
-		
-		Panel panel2 = new Panel();
-		panel2.setId(2);
-		panel2.setStatus(false);
-		panel2.setTitle("Wasserdings");
-		Action action2 = new Action();
-		action2.setId(12);
-		action2.setLabel("Freischalten");
-		panel2.getActions().add(action2);
-		panels.add(panel2);
-		return Response.status(Response.Status.OK).entity("TEST").build();
+
+		return Response.status(Response.Status.OK).entity(new Panel()).build();
 	}
 
 	@Override
 	public Response swapPanes(Integer pid1, Integer pid2, SecurityContext securityContext) {
 		// TODO Auto-generated method stub
-		return null;
+		return Response.status(Response.Status.OK).entity("TEST").build();
 	}
 
 }
