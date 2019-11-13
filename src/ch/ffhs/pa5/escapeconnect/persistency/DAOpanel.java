b0e39ba99f746d5a0239f42de6687078a144d74c
Ludovic Renevey <renevey.ludovic@gmail.com>
2019-11-13 12:34:40 +0100

    Add the functions getAllPanels, getByID, getByDeviceID

--- src/ch/ffhs/pa5/escapeconnect/persistency/DAOpanel.java
+++ src/ch/ffhs/pa5/escapeconnect/persistency/DAOpanel.java
@@ -4,8 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.WebApplicationException;
 
+import ch.ffhs.pa5.escapeconnect.bean.Panel;
 import ch.ffhs.pa5.escapeconnect.bean.PanelDAOBean;
 
 public class DAOpanel implements DAOpanelIF {
@@ -52,4 +56,94 @@ public class DAOpanel implements DAOpanelIF {
 		}
 		return -1;
 	}
+	
+	public List<PanelDAOBean> getAllPanels() {
+		
+		// start the connection with the BD
+		String query = "SELECT * FROM panel";
+		Connection con = DBAdapter.getConnection();
+		// We use an ArrayList so that TomCat can easily convert it to a JSON.
+		List<PanelDAOBean> list_panels = new ArrayList<>();
+		try (PreparedStatement pstm = con.prepareStatement(query)){
+			ResultSet rs = pstm.executeQuery();
+			// if the DB has no Panels, skip the code with the if-statement
+			// Take the ResultSet rs and iterate through it in order to create the beans "Panel"
+			if(rs.next() != false) {	
+				do {
+					PanelDAOBean generated_panel = new PanelDAOBean();
+					generated_panel.setId(rs.getInt("id"));
+					generated_panel.setName(rs.getString("name"));
+					list_panels.add(generated_panel);
+				} while (rs.next());	
+			}
+			// Close the connection to the DB
+			pstm.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new WebApplicationException(e.getMessage());
+		}	
+		// Return the list of beans "Panel" or empty.
+		return list_panels;
+	}
+	
+	public List<PanelDAOBean> getByDevice(int deviceId) {
+		
+		// start the connection with the BD
+		String query = "SELECT * FROM panel WHERE device_id = ?";
+		Connection con = DBAdapter.getConnection();
+		// We use an ArrayList so that TomCat can easily convert it to a JSON.
+		List<PanelDAOBean> single_panel = new ArrayList<>();
+		try (PreparedStatement pstm = con.prepareStatement(query)){
+			pstm.setInt(1, deviceId);
+			ResultSet rs = pstm.executeQuery();
+			// Take the ResultSet rs and get the first line.
+			// if nothing, next() returns false. We do check this first.
+			if(rs.next() != false) {
+				PanelDAOBean generated_panel = new PanelDAOBean();
+				generated_panel.setId(rs.getInt("id"));
+				generated_panel.setTitle(rs.getString("name"));
+				single_panel.add(generated_panel);
+			}
+			// Close the connection to the DB
+			pstm.close(); 
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new WebApplicationException(e.getMessage());
+		}
+		// Return the rs with the panel or empty.
+		return single_panel;			
+	}
+	
+	public List<PanelDAOBean> getById(int id) {
+		// start the connection with the BD
+		String query = "SELECT * FROM panel WHERE id = ?";
+		Connection con = DBAdapter.getConnection();
+		// We use an ArrayList so that TomCat can easily convert it to a JSON.
+		List<PanelDAOBean> single_panel = new ArrayList<>();
+		try (PreparedStatement pstm = con.prepareStatement(query)){
+			pstm.setInt(1, id);
+			ResultSet rs = pstm.executeQuery();
+			// Take the ResultSet rs and get the first line.
+			// if nothing, next() returns false. We do check this first.
+			if(rs.next() != false) {
+				PanelDAOBean generated_panel = new PanelDAOBean();
+				generated_panel.setId(rs.getInt("id"));
+				generated_panel.setTitle(rs.getString("name"));
+				single_panel.add(generated_panel);
+			}
+			// Close the connection to the DB
+			pstm.close(); 
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new WebApplicationException(e.getMessage());
+		}
+		// Return the rs with the panel or empty.
+		return single_panel;			
+	}
+	
+	public void delete(int id) {
+		
+	}
+	
+	
 }
