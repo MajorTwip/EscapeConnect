3e8df14a98fe13d320095850005d8d6b7d5f2861
MajorTwip <majortwip@twipnet.ch>
2019-11-14 12:26:12 +0100

    Transferred Docker-Creation to GitHub Actions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cp target/*.war ./ROOT.war
 cp schemas/api.yaml swagger-dist/api.yaml
-docker build -t majortwip/escapeconnect:latest .
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push majortwip/escapeconnect
+# Docker creation transferred to GitHub Actions
+#docker build -t majortwip/escapeconnect:latest .
+#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+#docker push majortwip/escapeconnect
 #docker build -t majortwip/escapeconnect:arm32v7 -f Dockerfile-arm32v7 .
 #docker push majortwip/escapeconnect
