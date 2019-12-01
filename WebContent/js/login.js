23171dfb16bd4c34423c7ce888d6a2b84f386a81
MajorTwip <majortwip@twipnet.ch>
2019-12-01 14:22:45 +0100

    Implemented basic login-functionnality, closes #21

---
+++ WebContent/js/login.js
@@ -0,0 +1,32 @@
+$("#btn_login").bind("click", login);
+
+function login() {
+	var pass = $("#txt_pw").val().trim();
+	if (pass.length==0) {
+		alert("Passwortfeld leer");
+		return;
+	}
+	try {
+		var json = new Object();
+		json.passhash=pass;
+		  const resp = postJson('rest/admin/login', json);
+		  console.log((resp));
+		} catch (error) {
+		  alert(error);
+		}
+}
+
+async function postJson(url = '', data = {}) {
+	  const response = await fetch(url, {
+	    method: 'POST', 
+	    headers: {
+	      'Content-Type': 'application/json'
+	    },
+	    body: JSON.stringify(data) 
+	  });
+	  if(response.ok){
+		  alert("Login successful");
+	  }
+	  if(response.status==409)
+		  alert("Password wrong");
+	}
