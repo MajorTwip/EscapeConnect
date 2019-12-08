5d55c8a45a9312befc07305640db8b75f0137cfa
MajorTwip <majortwip@twipnet.ch>
2019-12-08 13:27:14 +0100

    Implement setting/get (#54)

--- src/main/java/ch/ffhs/pa5/escapeconnect/handlers/SettingAPIimplement.java
+++ src/main/java/ch/ffhs/pa5/escapeconnect/handlers/SettingAPIimplement.java
@@ -9,29 +9,43 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
 import ch.ffhs.pa5.escapeconnect.api.SettingApiService;
+import ch.ffhs.pa5.escapeconnect.bean.Setting;
+import ch.ffhs.pa5.escapeconnect.bean.SettingDAOBean;
 import ch.ffhs.pa5.escapeconnect.bean.SettingMod;
 import ch.ffhs.pa5.escapeconnect.mqtt.MQTTconnector;
+import ch.ffhs.pa5.escapeconnect.persistency.DAOsettings;
 
 public class SettingAPIimplement implements SettingApiService {
+	
+	DAOsettings daosettings = new DAOsettings();
+	
 
 	@Override
-	public Response getSettingsByDeviceId(Integer deviceId, SecurityContext securityContext) {
+	public Response getSettingsByPanelId(Integer panelId, SecurityContext securityContext) {
 		
-		
-		
-		//Bullshitimplementation nur zum testen ob die MQTTfunktion klappt
-		MQTTconnector mqtt = new MQTTconnector("tcp://mqtt.comstock.ch", null, null);
-		LinkedList<String> topics = new LinkedList<>();
-		topics.add("csh/84f3eb7b2d65/generator/state");
-		topics.add("shellies/shellyswitch25-692F79/relay/0");
-		Map<String,String> resp = mqtt.getMessages(topics, 1000);
-		return Response.status(200).entity(resp).build();
+		if(panelId==null) {
+			return Response.status(Response.Status.CONFLICT).entity("no panelId provided").build();
+		}
+		List<SettingDAOBean> settings = daosettings.getSettingsByPanelId(panelId);
+		List<Setting> settingsresponse = new LinkedList<>();
+		for(SettingDAOBean set:settings) {
+			Setting respset = new Setting();
+			respset.setId(set.getId());
+			respset.setLabel(set.getLabel());
+			respset.setMax(String.valueOf(set.getMax()));
+			respset.setMin(String.valueOf(set.getMin()));
+			respset.setType(Setting.TypeEnum.fromValue(set.getType()));
+			respset.setValue(set.getValue());
+			settingsresponse.add(respset);
+		}
+	
+		return Response.status(200).entity(settingsresponse).build();
 	}
 
 	@Override
-	public Response setSetting(List<SettingMod> body, Integer deviceId, SecurityContext securityContext) {
+	public Response setSetting(List<SettingMod> body, SecurityContext securityContext) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-}
+} 
