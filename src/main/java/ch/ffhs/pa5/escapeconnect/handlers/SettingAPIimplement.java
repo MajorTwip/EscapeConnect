656e281d4150147047531b39cdbc3ea0af65b541
MajorTwip <majortwip@twipnet.ch>
2019-12-14 21:12:12 +0100

    Implemented Settings/get (#58)

--- src/main/java/ch/ffhs/pa5/escapeconnect/handlers/SettingAPIimplement.java
+++ src/main/java/ch/ffhs/pa5/escapeconnect/handlers/SettingAPIimplement.java
@@ -9,16 +9,28 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
 import ch.ffhs.pa5.escapeconnect.api.SettingApiService;
+import ch.ffhs.pa5.escapeconnect.bean.DeviceDAOBean;
+import ch.ffhs.pa5.escapeconnect.bean.EcSettings;
+import ch.ffhs.pa5.escapeconnect.bean.PanelDAOBean;
 import ch.ffhs.pa5.escapeconnect.bean.Setting;
 import ch.ffhs.pa5.escapeconnect.bean.SettingDAOBean;
 import ch.ffhs.pa5.escapeconnect.bean.SettingMod;
 import ch.ffhs.pa5.escapeconnect.mqtt.MQTTconnector;
+import ch.ffhs.pa5.escapeconnect.persistency.DAOdevice;
+import ch.ffhs.pa5.escapeconnect.persistency.DAOecsettings;
+import ch.ffhs.pa5.escapeconnect.persistency.DAOpanel;
 import ch.ffhs.pa5.escapeconnect.persistency.DAOsettings;
+import ch.ffhs.pa5.escapeconnect.utils.ParseSettingsJSON;
 
 public class SettingAPIimplement implements SettingApiService {
 	
 	DAOsettings daosettings = new DAOsettings();
+	DAOpanel daopanel = new DAOpanel();
+	DAOdevice daodevice = new DAOdevice();
+	DAOecsettings daoecsettings = new DAOecsettings();
+	MQTTconnector mqtt = new MQTTconnector();
 	
+
 
 	@Override
 	public Response getSettingsByPanelId(Integer panelId, SecurityContext securityContext) {
@@ -26,6 +38,26 @@ public class SettingAPIimplement implements SettingApiService {
 		if(panelId==null) {
 			return Response.status(Response.Status.CONFLICT).entity("no panelId provided").build();
 		}
+		
+		//get current config from device
+		PanelDAOBean panel = daopanel.getById(panelId);
+		
+		DeviceDAOBean device = daodevice.getByMac(panel.getDevice_mac());
+		
+		EcSettings ecsettings = daoecsettings.get();
+		
+		mqtt.config(ecsettings.getMqttUrl(), ecsettings.getMqttName(), ecsettings.getMqttPass());
+		
+		String settingstopic = String.join("/", device.getBasetopic(),device.getDeviceid(),"$implementation/config");
+		List<String> topics = new LinkedList<>();
+		topics.add(settingstopic);
+		
+		Map<String,String> results = mqtt.getMessages(topics, 1000);
+		
+		String settingsjson = results.get(settingstopic);
+		
+		Map<String,String> settingsactual = ParseSettingsJSON.parseJSON(settingsjson);
+		
 		List<SettingDAOBean> settings = daosettings.getSettingsByPanelId(panelId);
 		List<Setting> settingsresponse = new LinkedList<>();
 		for(SettingDAOBean set:settings) {
@@ -36,9 +68,21 @@ public class SettingAPIimplement implements SettingApiService {
 			respset.setMin(String.valueOf(set.getMin()));
 			respset.setType(Setting.TypeEnum.fromValue(set.getType()));
 			respset.setValue(set.getValue());
+			
+			if(settingsactual.containsKey(set.getName())) {
+				String value = settingsactual.get(set.getName());
+				respset.setValue(value);
+				set.setValue(value);
+				daosettings.write(set);
+			}
+			
 			settingsresponse.add(respset);
 		}
-	
+		
+
+		
+		
+		
 		return Response.status(200).entity(settingsresponse).build();
 	}
 
